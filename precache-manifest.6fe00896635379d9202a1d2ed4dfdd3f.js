3884e0aac309d318ffd2aa1b51e523e5c56085fb
Sabrina <sabrina.muniz@icloud.com>
2020-01-09 12:39:38 -0500

    Updates

---
+++ precache-manifest.6fe00896635379d9202a1d2ed4dfdd3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb800e84702809dc6cc96122a332a646",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "53cfb8be8f2c2c1f3150",
+    "url": "/clicky-game/static/css/main.62e5c2cf.chunk.css"
+  },
+  {
+    "revision": "5c3f4d58afe38700c6cb",
+    "url": "/clicky-game/static/js/2.7463f86d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.7463f86d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53cfb8be8f2c2c1f3150",
+    "url": "/clicky-game/static/js/main.359fb549.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  }
+]);
